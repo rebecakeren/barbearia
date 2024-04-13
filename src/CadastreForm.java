64040c6cb9ccd880c7c308c8c6a2851eefda8fde
rebecakeren <rebecakeren.privado@gmail.com>
2024-04-13 11:36:14 -0300

    v1.0

---
+++ src/CadastreForm.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class CadastreForm {
+    private JPanel cadastreForm;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JPasswordField passwordField1;
+    private JButton btnCadastre;
+    private JComboBox comboBox;
+    private JButton btnCancel;
+}
---
+++ src/SchedulingPage.java
@@ -0,0 +1,2 @@
+public class SchedulingPage {
+}
